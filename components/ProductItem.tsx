12c65e6994d3184f2e5d7e3d6c55f3c203a59ac2
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-06-29 08:17:07 -0300

    implementing memo

---
+++ components/ProductItem.tsx
@@ -0,0 +1,29 @@
+import { memo } from "react"
+
+
+interface ProductItemProps {
+    products: {
+        id: number;
+        price: number;
+        // title: string;
+    }
+}
+
+function ProductItemComponent({ product }) {
+    return (
+        <div>
+            {product.title} = <strong>{product.price}</strong>
+        </div>
+    )
+}
+
+// O memo evita que o React crie uma nova versão do componente, se ele não tiver nenhuma prop alterada.
+// O mesmo faz uma shadow compare(comparação rasa).
+// Quando uma prop tem objetos ou algo que a comparação pode ser feita usando ===, o memo precisa de um segundo parâmetro.
+// prevProps = são propriedades que eu tinha antes da renderização
+// nextProps = novas propriedades depois da nova renderização
+// O Object.is compara cada uma das props em ProductItemProps. Deve ser usado com cuidado, apenas se os dados retornados não forem muito complexos.
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+   return Object.is(prevProps.product, nextProps.product)
+})
